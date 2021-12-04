2a1e4564702e8c9dd086c6cc08a35f7daa923403
Gordon Hamilton <gordon@Gordons-MacBook-Air.local>
2021-12-04 11:24:28 -0500

    fixed form and deleted unnessary code

--- Phase03/clientSide.js
+++ Phase03/clientSide.js
@@ -2,7 +2,7 @@
 
 var url = "http://localhost:3000/post";
 
-function sendToPage(){
+function sendToPage() {
     var input = document.getElementById("search").value.toLowerCase();
     retrieve(input);
 }
@@ -20,119 +20,9 @@ function response(data, status){
     console.log("this is res: ", res);
     // Blanks out the list of definitions on the homepage
     document.getElementById("list").innerHTML = "";
-    
+
     // adds in the specific definitions data from the server
     document.getElementById("title").innerHTML = res['name'];
     document.getElementById("p").innerHTML = res['def'];
     console.log('sent to server');
-}
-
-function axiomDef(){
-   
-    var toSend = {
-        title: 'Axiom',
-       
-
-    }
-
-    var jsonString = JSON.stringify(toSend);
-    console.log(jsonString);
-    $.post(url+'?data='+jsonString, response1);
-
-
-    
-}
-
-
-function biDef(){
-   
-    var toSend = {
-        title: 'Bi',
-       
-
-    }
-
-    var jsonString = JSON.stringify(toSend);
-    console.log(jsonString);
-    $.post(url+'?data='+jsonString, response2);
-
-
-    
-}
-function response2(data, status){
-    var res = JSON.parse(data);
-    console.log(data);
-
-    document.getElementsByClassName("content").innerHTML = " ";
-
-    document.getElementById("p2").innerHTML = res['def'];
-}
-
-function compoundDef(){
-   
-    var toSend = {
-        title: 'Compound',
-       
-
-    }
-
-    var jsonString = JSON.stringify(toSend);
-    console.log(jsonString);
-    $.post(url+'?data='+jsonString, response3);
-
-
-    
-}
-function response3(data, status){
-    var res = JSON.parse(data);
-    console.log(data);
-
-    
-    document.getElementsByClassName("content").innerHTML = " ";
-
-    document.getElementById("p3").innerHTML = res['def'];
-}
-
-function compoundStatementDef(){
-   
-    var toSend = {
-        title: 'CompoundStatement',
-       
-
-    }
-
-    var jsonString = JSON.stringify(toSend);
-    console.log(jsonString);
-    $.post(url+'?data='+jsonString, response4);
-
-
-    
-}
-function response4(data, status){
-    var res = JSON.parse(data);
-    console.log(data);
-
-    document.getElementsByClassName("content").innerHTML = " ";
-
-    document.getElementById("p4").innerHTML = res['def'];
-}
-function conjunction(){
-    var toSend = {
-        title: 'Conjunction',
-       
-
-    }
-
-    var jsonString = JSON.stringify(toSend);
-    console.log(jsonString);
-    $.post(url+'?data='+jsonString, response5);
-
-}
-function response5(data, status){
-    var res = JSON.parse(data);
-    console.log(data);
-
-    
-
-    document.getElementById("p5").innerHTML = res['def'];
 }
